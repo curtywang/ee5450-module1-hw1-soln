a7039f81a4d2757d50ca0bdd90bff6923c5f94cc
Curtis Wang <ycwang@u.northwestern.edu>
2021-02-10 17:32:49 -0800

    initial commit

--- test_web_blackjack.py
+++ test_web_blackjack.py
@@ -1,10 +1,27 @@
-import unittest
+import pytest
+from fastapi.testclient import TestClient
+from web_blackjack import app
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+@pytest.fixture
+def base_client():
+    return TestClient(app)
+
+
+def test_home(base_client):
+    response = base_client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to Blackjack!"}
+
+
+def test_create(base_client):
+    response = base_client.get('/game/create/1')
+    response_json = response.json()
+    assert response.status_code == 201
+    assert response_json['success'] is True
+    assert len(response_json['game_id']) == 36
+    assert len(response_json['termination_password']) == 36
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
